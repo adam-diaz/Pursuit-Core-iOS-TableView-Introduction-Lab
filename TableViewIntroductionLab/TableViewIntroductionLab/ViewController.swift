73d245cc364a2573657f6ea7b3c187a990e8d15b
Adán Diaz <adamdiaz@pursuit.org>
2019-11-12 17:27:05 -0500

    added tableview with constraints, prototype cell, navigation control connected UBOutlet and private tasks variable

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var tasks = [Task]() {
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
